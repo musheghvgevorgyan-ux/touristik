b84dd265d2d298e6bf962eb8ea98d248bd978a5e
Mushegh <musheghvgevorgyan@gmail.com>
2026-04-04 15:00:19 +0400

    Remove deploy script

--- laravel/deploy_update.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo shell_exec('php ' . __DIR__ . '/artisan migrate --force 2>&1');
-echo shell_exec('php ' . __DIR__ . '/artisan config:cache 2>&1');
-echo shell_exec('php ' . __DIR__ . '/artisan route:cache 2>&1');
-echo shell_exec('php ' . __DIR__ . '/artisan view:cache 2>&1');
-echo "\nDONE!\n";
