c43a15d8e59e2506606f98f88c106992fa4a51ba
Mushegh <musheghvgevorgyan@gmail.com>
2026-04-04 14:16:12 +0400

    Remove temporary deployment setup script

--- laravel/setup_env.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Reset migrations and re-run
-echo shell_exec('php ' . __DIR__ . '/artisan migrate:fresh --force 2>&1');
-echo shell_exec('php ' . __DIR__ . '/artisan db:seed --force 2>&1');
-echo shell_exec('php ' . __DIR__ . '/artisan config:cache 2>&1');
-echo shell_exec('php ' . __DIR__ . '/artisan route:cache 2>&1');
-echo shell_exec('php ' . __DIR__ . '/artisan view:cache 2>&1');
-echo "\nDONE!\n";
